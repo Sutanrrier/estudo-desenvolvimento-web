47b9a8c772b0c05f39040a2f8792acf49fe316cc
Sutanrrier <pabloryanbr0@gmail.com>
2022-05-30 16:12:03 -0300

    JS - Exercicio 02

---
+++ JS/exercicios/exercicio2.js
@@ -0,0 +1,8 @@
+//Exercicio - Crie uma função que retorna o maior número dentre os fornecidos pelo parametro.
+
+//Solução
+function maiorNumero(num1, num2) {
+    return (num1 > num2) ? num1 : num2;
+}
+
+console.log(maiorNumero(3, 10));
