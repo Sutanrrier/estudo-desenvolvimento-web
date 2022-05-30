6899c88744b31283d1a0fdad77e8597085d6e700
Sutanrrier <pabloryanbr0@gmail.com>
2022-05-30 14:31:31 -0300

    JS - Variaveis/Constantes/Objetos/Arrays/Funcoes

---
+++ JS/estudo.js
@@ -0,0 +1,25 @@
+//Variaveis tipo primitivo
+let idade = 21;
+let altura = 1.71;
+let nome = "Pablo"
+console.log(idade, altura, nome);
+
+//Constante
+const preco = 2.01;
+console.log("Preço: ", preco)
+
+//Criando um objeto
+let pessoa = {
+    nome: "Pablo",
+    altura: 1.71,
+    idade: 21
+};
+
+//Criando um array
+let array2 = [1,1.22,"Arnoldo", true];
+
+//Criando função
+let corSite = "Azul";
+function resetaCor(cor){
+    corSite = cor;
+}
