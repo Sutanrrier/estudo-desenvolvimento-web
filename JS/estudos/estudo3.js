bd12dc5e2b1aaa69bd402014a8ddb635b559dc0c
Sutanrrier <pabloryanbr0@gmail.com>
2022-05-31 14:19:23 -0300

    JS - Factory Functions/Constructor Functions/Cloning Objects

---
+++ JS/estudos/estudo3.js
@@ -0,0 +1,32 @@
+//Factory Function
+function criarPessoa(nome, idade, altura){
+    return {
+        nome,
+        idade,
+        altura,
+        mostrarInfo(){
+            console.log(nome + " " + idade + " " + altura);
+        }
+    }
+}
+
+const pessoa1 = criarPessoa("José", 33, 2.01);
+pessoa1.mostrarInfo();
+
+//constructor Function
+function Pessoa(nome, idade, altura){
+    this.nome = nome,
+    this.idade = idade,
+    this.altura = altura,
+    this.mostrarInfo = function(){
+        console.log(nome + " " + idade + " " + altura);
+    }
+}
+
+const pessoa2 = new Pessoa("Maria", 22, 1.81);
+pessoa2.mostrarInfo();
+
+
+//Clonando objetos
+const pessoaClone = {...pessoa2};
+pessoaClone.mostrarInfo();
