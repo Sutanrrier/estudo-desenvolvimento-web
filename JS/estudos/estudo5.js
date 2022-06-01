bb4d358cc33a607ff696a338a916449493beda9f
Sutanrrier <pabloryanbr0@gmail.com>
2022-06-01 16:57:12 -0300

    JS - Estudos iniciais sobre DOM

---
+++ JS/estudos/estudo5.js
@@ -0,0 +1,8 @@
+//Como capturar input de usuario - prompt()
+let corFavorita = prompt("Insira sua cor favorita!");
+
+//Exibindo um alerta na tela - alert()
+alert("Sua cor favorita é " + corFavorita);
+//-----------------------------------------
+
+//DOM
